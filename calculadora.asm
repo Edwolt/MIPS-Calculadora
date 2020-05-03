474d6f8e12684ecccbffad96d0a2bad37f77e784
Edws <eduardos.rocha17@gmail.com>
2020-05-03 16:40:00 -0300

    Subtração Multiplicação Divisão
    Modularização do código
    Unsando labels mais significativas
    Operações de subtração, multiplicação e divisão

--- calculadora.asm
+++ calculadora.asm
@@ -1,125 +1,191 @@
           .data
           .align 0
-message1: .asciiz "Escolha uma opção: \n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Potência\n6-Raiz Quadrada\n7-Tabuada\n8-IMC\n9-Fatorial\n10-Sequência de Fibonacci\n0-Sair\n"         
-message2: .asciiz "Digite um número inteiro:\n"
-message3: .asciiz "Digite outro número inteiro:\n"
-message4: .asciiz "O resultado é: "
-message5: .asciiz "\n\n"
+msg_menu: .asciiz "Escolha uma opção: \n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Potência\n6-Raiz Quadrada\n7-Tabuada\n8-IMC\n9-Fatorial\n10-Sequência de Fibonacci\n0-Sair\n"         
+msg_num1: .asciiz "Digite um número inteiro:\n"
+msg_num2: .asciiz "Digite outro número inteiro:\n"
+msg_result: .asciiz "O resultado é: "
+msg_nn: .asciiz "\n\n"
 
-          .text
-          .globl main 
+        .text
+	.globl main 
 
 main:
-         j menu  
+        j menu  
 menu:
-         li $v0, 4
-         la $a0, message1
-         syscall
+        li $v0, 4
+        la $a0, msg_menu
+        syscall
          
-         li $v0, 5
-         syscall
+        li $v0, 5
+        syscall
          
-         beq $v0, 1, soma
-         beq $v0, 2, subtracao
-         beq $v0, 3, multiplicacao
-         beq $v0, 4, divisao
-         beq $v0, 5, potencia
-         beq $v0, 6, raiz
-         beq $v0, 7, multiplicacao
-         beq $v0, 8, divisao
-         beq $v0, 9, potencia
-         beq $v0, 10, raiz
-         beq $v0, 0, sair
+        beq $v0, 1, soma
+        beq $v0, 2, subtracao
+        beq $v0, 3, multiplicacao
+        beq $v0, 4, divisao
+        beq $v0, 5, potencia
+        beq $v0, 6, raiz
+        beq $v0, 7, multiplicacao
+        beq $v0, 8, divisao
+        beq $v0, 9, potencia
+        beq $v0, 10, raiz
+        beq $v0, 0, sair
 
-	 j menu
+	j menu
 soma:
-         li $v0, 4
-         la $a0, message2
-         syscall
+        jal ler_num1        
+        move $s0, $v0
+        
+        jal ler_num2
+        move $s1, $v0
+        
+        add $a0, $s0, $s1
+        
+        jal print_result
          
-         li $v0, 5
-         syscall
-         
-         move $t0, $v0
-         
-         li $v0, 4
-         la $a0, message3
-         syscall
-         
-         li $v0, 5
-         syscall
-         
-         move $t1, $v0
-         
-         add $t2, $t0, $t1
-         
-         li $v0, 4
-         la $a0, message4
-         syscall
-         
-         li $v0, 1
-         move $a0, $t2
-         syscall
-         
-         li $v0, 4
-         la $a0, message5
-         syscall
-         
-         j menu
+        j menu
+
 subtracao:
-         li $v0, 4
-         la $a0, message2
-         syscall
+	jal ler_num1
+	move $s0, $v0
+	
+	jal ler_num2
+	move $s1, $v0
+	
+	sub $a0, $s0, $s1
+	
+	jal print_result
          
-         j menu
+        j menu
+
 multiplicacao:
-         li $v0, 4
-         la $a0, message2
-         syscall
-         
-         j menu
+        jal ler_num1
+        move $s0, $v0
+	
+	jal ler_num2
+	move $s1, $v0
+	
+	mult $s0, $s1
+	mflo $a0
+	
+	jal print_result
+	
+	j menu
+
 divisao:
-         li $v0, 4
-         la $a0, message2
-         syscall
+        jal ler_num1
+        move $s0, $v0
+	
+	jal ler_num2
+	move $s1, $v0
+	
+	div $s0, $s1
+	mflo $a0
+	
+	jal print_result
          
-         j menu
+        j menu
+
 potencia:
-         li $v0, 4
-         la $a0, message2
-         syscall
-         
-         j menu
+        jal ler_num1
+        
+        j menu
+
 raiz:
-         li $v0, 4
-         la $a0, message2
-         syscall
+        jal ler_num1
          
-         j menu
+        j menu
+
 tabuada:
-         li $v0, 4
-         la $a0, message2
-         syscall
+        jal ler_num1
          
-         j menu
+        j menu
+
 imc:
-         li $v0, 4
-         la $a0, message2
-         syscall
+        jal ler_num1
          
-         j menu
+        j menu
+
 fatorial:
-         li $v0, 4
-         la $a0, message2
-         syscall
+        jal ler_num1
          
-         j menu
+        j menu
+
 fibonacci:
-         li $v0, 4
-         la $a0, message2
-         syscall
-         
-         j menu
+        jal ler_num1
+                 
+        j menu
+
 sair:
-         li $v0, 10
-         syscall
+        li $v0, 10
+        syscall
+   
+# Imprime número pelo argumento $a0      
+print_result:
+	# Salva $a0 em $t0 pois $a0 será usado
+	move $t0, $a0
+	
+	# Empilha
+	subi $sp, $sp, 8
+	sw $ra, 4($sp)
+	sw $a0, 0($sp)
+
+	# Imprime string msg_result
+	li $v0, 4
+        la $a0, msg_result
+        syscall
+        
+        # Imprime int que veio de argumento
+        li $v0, 1
+        move $a0, $t0
+        syscall
+        
+        # Imprime espaço vazio
+        li $v0, 4
+        la $a0, msg_nn
+        syscall
+        
+        # Desempilha
+	lw $a0, 0($sp)
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
+	
+	jr $ra
+
+# Le um número e retorna para o $v0
+ler_num1:
+	subi $sp, $sp, 8
+	sw $ra, 4($sp)
+	sw $a0, 0($sp)
+	
+	li $v0, 4
+        la $a0, msg_num1
+        syscall
+ 
+        li $v0, 5
+        syscall
+
+	lw $a0, 0($sp)
+	lw $ra, 4($sp)	
+	addi $sp, $sp, 8
+	
+	jr $ra
+
+# Le um número e retorna para o $v0
+ler_num2:
+	subi $sp, $sp, 8
+	sw $ra, 4($sp)
+	sw $a0, 0($sp)
+	
+	li $v0, 4
+        la $a0, msg_num2
+        syscall
+ 
+        li $v0, 5
+        syscall
+
+	lw $a0, 0($sp)
+	lw $ra, 4($sp)	
+	addi $sp, $sp, 8
+	
+	jr $ra
