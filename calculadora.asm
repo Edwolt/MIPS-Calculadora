0cae8c65076a3dac3332471606d04ef33c8178c7
Edws <eduardos.rocha17@gmail.com>
2020-05-03 17:37:50 -0300

    Fatorial

--- calculadora.asm
+++ calculadora.asm
@@ -1,125 +1,141 @@
-          .data
-          .align 0
-msg_menu: .asciiz "Escolha uma opção: \n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Potência\n6-Raiz Quadrada\n7-Tabuada\n8-IMC\n9-Fatorial\n10-Sequência de Fibonacci\n0-Sair\n"         
-msg_num1: .asciiz "Digite um número inteiro:\n"
-msg_num2: .asciiz "Digite outro número inteiro:\n"
-msg_result: .asciiz "O resultado é: "
-msg_nn: .asciiz "\n\n"
+	.data
+	.align 0
+msg_menu:	.asciiz "Escolha uma opção: \n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n5-Potência\n6-Raiz Quadrada\n7-Tabuada\n8-IMC\n9-Fatorial\n10-Sequência de Fibonacci\n0-Sair\n"         
+msg_num1:	.asciiz "Digite um número inteiro:\n"
+msg_num2:	.asciiz "Digite outro número inteiro:\n"
+msg_result:	.asciiz "O resultado é: "
+msg_nn:		.asciiz "\n\n"
 
-        .text
+	.text
 	.globl main 
 
 main:
-        j menu  
+	j menu  
 menu:
-        li $v0, 4
-        la $a0, msg_menu
-        syscall
-         
-        li $v0, 5
-        syscall
-         
-        beq $v0, 1, soma
-        beq $v0, 2, subtracao
-        beq $v0, 3, multiplicacao
-        beq $v0, 4, divisao
-        beq $v0, 5, potencia
-        beq $v0, 6, raiz
-        beq $v0, 7, multiplicacao
-        beq $v0, 8, divisao
-        beq $v0, 9, potencia
-        beq $v0, 10, raiz
-        beq $v0, 0, sair
+	li $v0, 4
+	la $a0, msg_menu
+	syscall
+
+	li $v0, 5
+	syscall
+
+	beq $v0, 1, soma
+	beq $v0, 2, subtracao
+	beq $v0, 3, multiplicacao
+	beq $v0, 4, divisao
+	beq $v0, 5, potencia
+	beq $v0, 6, raiz
+	beq $v0, 7, multiplicacao
+	beq $v0, 8, divisao
+	beq $v0, 9, fatorial
+	beq $v0, 10, raiz
+	beq $v0, 0, sair
 
 	j menu
 soma:
-        jal ler_num1        
-        move $s0, $v0
-        
-        jal ler_num2
-        move $s1, $v0
-        
+	jal ler_num1        
+	move $s0, $v0
+
+	jal ler_num2
+	move $s1, $v0
+
         add $a0, $s0, $s1
-        
-        jal print_result
-         
-        j menu
+
+	jal print_result
+
+	j menu
 
 subtracao:
 	jal ler_num1
 	move $s0, $v0
-	
+
 	jal ler_num2
 	move $s1, $v0
-	
+
 	sub $a0, $s0, $s1
-	
+
 	jal print_result
-         
-        j menu
+
+	j menu
 
 multiplicacao:
-        jal ler_num1
-        move $s0, $v0
-	
+	jal ler_num1
+	move $s0, $v0
+
 	jal ler_num2
 	move $s1, $v0
-	
+
 	mult $s0, $s1
 	mflo $a0
-	
+
 	jal print_result
-	
+
 	j menu
 
 divisao:
         jal ler_num1
         move $s0, $v0
-	
+
 	jal ler_num2
 	move $s1, $v0
-	
+
 	div $s0, $s1
 	mflo $a0
-	
+
 	jal print_result
-         
-        j menu
+
+	j menu
 
 potencia:
-        jal ler_num1
+	jal ler_num1
         
-        j menu
+	j menu
 
 raiz:
-        jal ler_num1
-         
-        j menu
+	jal ler_num1
+
+	j menu
 
 tabuada:
-        jal ler_num1
-         
-        j menu
+	jal ler_num1
+ 
+	j menu
 
 imc:
-        jal ler_num1
-         
-        j menu
+	jal ler_num1
+ 
+	j menu
 
 fatorial:
-        jal ler_num1
-         
-        j menu
+	jal ler_num1
+
+	move $t0, $v0
+	li $a0, 1   # Resultado do fatorial
+
+	# for(t0 = a0, t0 != 0, t0--)
+	loop_t0:
+		mult $a0, $t0
+		mflo $a0
+
+		subi $t0, $t0, 1
+
+		beqz $t0, loop_t0_fim
+		j loop_t0
+	loop_t0_fim:
+
+	jal print_result
+
+	j menu
 
 fibonacci:
-        jal ler_num1
-                 
-        j menu
+	jal ler_num1
+
+	j menu
 
 sair:
-        li $v0, 10
-        syscall
-   
+	li $v0, 10
+	syscall
+
 # Imprime número pelo argumento $a0      
 print_result:
 	# Salva $a0 em $t0 pois $a0 será usado
